02c8cd6f284c0a51ea04f9695c640542f1647732
刘航 <liuhang@kuainiugroup.com>
2018-02-08 17:23:45 +0800

    fix config py problems

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
+# -*- coding: utf-8 -*-
 class Config:
-
-"""
-配置类
-
-"""
+#配置类
     def __init__(self,df,feature_column_names,label_column_names,other_column_names,positive_label_value=1,output_dir=None):
         """
         feature_column_names: 特征列名列表 (list)
