6896d3fbce1d783f6ca23bfc95fdcbd7f8b60023
muzhenxu <muzhenxv@gmail.com>
2018-04-12 22:26:45 +0800

    fix bug

--- linear_model/xl_model.py
+++ linear_model/xl_model.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 class xl_model(object):
     def __init__(self, model_type='lr', task='binary', init=0.1, epoch=10, lr=0.1, k=4, reg_lambda=1.0, opt='sgd',
-                 metric='auc', cutoff=0):
+                 metric='auc', alpha=0.002, beta=0.8, lambda_1=0.001, lambda_2=1.0, cutoff=0):
         self.task = task
         self.init = init
         self.epoch = epoch
@@ -16,9 +16,14 @@ class xl_model(object):
         self.opt = opt
         self.metric = metric
         self.model_type = model_type
+        self.alpha = alpha
+        self.beta = beta
+        self.lambda_1 = lambda_1
+        self.lambda_2 = lambda_2
         self.cutoff = cutoff
-        self.params = {'task': task, 'init': init, 'epoch': epoch, 'lr': lr, 'k': k,
-                       'reg_lambda': reg_lambda, 'opt': opt, 'metric': metric}
+        self.params = {'task': self.task, 'init': self.init, 'epoch': self.epoch, 'lr': self.lr, 'k': self.k,
+                       'lambda': self.reg_lambda, 'opt': self.opt, 'metric': self.metric, 'beta': self.beta,
+                       'alpha': self.alpha, 'lambda_1': self.lambda_1, 'lambda_2': self.lambda_2}
 
     def fit(self, df, label, eva_df=None, eva_label=None, path='datasource/train.ffm', eva_path='datasource/valid.ffm',
             model_path='datasource/ffm_model.out'):
@@ -38,7 +43,7 @@ class xl_model(object):
         self.fe.fit(df, self.cutoff)
         self.fe.transform(df, label, path)
         if eva_df is not None:
-            self.transform(eva_df, eva_label, eva_path)
+            self.fe.transform(eva_df, eva_label, eva_path)
 
         self.clf.setTrain(path)
         if eva_df is not None:
@@ -54,6 +59,7 @@ class xl_model(object):
         self.clf.predict(self.model_path, out_path)
         pred = pd.read_csv(out_path)
         return pred
+
 
 if __name__ == '__main__':
     pass
